03a8d0f4815c1bdb99e591abc2fb09710c0e4821
kamilK <kamil.kukowski@gmail.com>
2021-03-15 23:48:34 +0100

    fix[enviroment] -  fixed API_BASE for prod enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  API_BASE: 'https://jsonplaceholder.typicode.com',
+  API_BASE: 'https://api.github.com/users/Kamil-Kuk',
+  // API_BASE: 'https://jsonplaceholder.typicode.com',
   env: 'prod',
   WEATHER_API_KEY: '1dedf2653bebce54f343ee4eea900abd',
   WEATHER_API_BASE: 'https://api.openweathermap.org/data/2.5',
